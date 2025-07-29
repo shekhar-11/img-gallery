d7bb94af6d25188c6f6a5274dbed8edaa707359d
shekhar-11 <rajshekhar111201@gmail.com>
2025-07-29 13:02:43 +0530

    signup page ui

--- src/Pages/Signup.tsx
+++ src/Pages/Signup.tsx
@@ -4,8 +4,29 @@ export const Signup = ()=>{
 
     return (
 
-        <>
-        <h1>Hiii SIgnup</h1>
-        </>
+        <form>
+        <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col ">
+    <div className="text-center ">
+      <h1 className="text-5xl font-bold">IMG-Gallery</h1>
+      <p className="py-6">
+        Sign Up To Enter The Gallery
+      </p>
+    </div>
+    <div className="card bg-base-100 sm:w-[30rem] shadow-2xl">
+      <div className="card-body">
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </div>
+    </div>
+  </div>
+</div>
+        </form>
     )
 }
